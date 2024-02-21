1c25baeb94462c0394cfcfa80d36f6604077d8d9
ArthurVS05 <arthur.vincentsil@gmail.com>
2024-02-21 11:09:48 +0100

    correction schema.sql et connection BDD

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,11 +1,9 @@
--- Table du template du repo
 
 create table item (
   id int unsigned primary key auto_increment not null,
   title varchar(255) not null
 );
 
--- Notre database
 
 CREATE TABLE user (
   u_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -20,37 +18,19 @@ CREATE TABLE group_table (
    g_name VARCHAR(20) NOT NULL
 );
 
---table d'association user et group
-
 CREATE TABLE user_group (
 ug_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 ug_user_id INT NOT NULL,
 ug_group_id INT NOT NULL,
 ug_message TEXT NOT NULL,
--- à checker
-CONSTRAINT fk_user-group_user
+
+CONSTRAINT fk_user_group_user
     FOREIGN KEY (ug_user_id)
     REFERENCES user(u_id),
-CONSTRAINT fk_user-group_group
+CONSTRAINT fk_user_group_group
   FOREIGN KEY (ug_group_id)
   REFERENCES group_table(g_id)    
 );
-
---test de requête mySQL : récupérer les users, leur groupe et leur message :
--- mysql> SELECT u.name AS user_name, g.name AS group_name, ug.message
---     -> FROM user_group AS ug
---     -> JOIN user AS u ON u.id = ug.user_id
---     -> JOIN group_table AS g ON g.id = ug.group_id;
-
--- exemple (avec fake data ChatGTP)
--- +---------------+------------+---------------------------------+
--- | user_name     | group_name | message                         |
--- +---------------+------------+---------------------------------+
--- | John Doe      | Group A    | Hello from John to Group A      |
--- | Jane Smith    | Group A    | Message from Jane to Group A    |
--- | Alice Johnson | Group B    | Greetings from Alice to Group B |
--- +---------------+------------+---------------------------------+
--- 3 rows in set (0.00 sec)
 
 CREATE TABLE category_document (
     cd_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -58,7 +38,6 @@ CREATE TABLE category_document (
     cd_private BOOLEAN NOT NULL
 );
 
--- ajouter un timestamp si on veut trier les docs par date de création
 CREATE TABLE document (
   d_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   d_name VARCHAR(50) NOT NULL,
@@ -91,7 +70,7 @@ CREATE TABLE transaction (
     tr_cat_transaction_id INT NOT NULL,
     tr_group_id INT NOT NULL, 
     tr_user_id INT NOT NULL, 
-        CONSTRAINT fk_transaction_category-trans
+        CONSTRAINT fk_transaction_category_trans
             FOREIGN KEY (tr_cat_transaction_id)
             REFERENCES category_transaction(ctra_id),
         CONSTRAINT fk_transaction_group
@@ -116,7 +95,7 @@ CREATE TABLE task (
     ta_cat_task_id INT NOT NULL,
     ta_user_id INT NOT NULL,
     ta_group_id INT NOT NULL,
-        CONSTRAINT fk_task_category-task
+        CONSTRAINT fk_task_category_task
             FOREIGN KEY (ta_cat_task_id)
             REFERENCES category_task(cta_id),
         CONSTRAINT fk_task_user
@@ -127,16 +106,6 @@ CREATE TABLE task (
             REFERENCES group_table(g_id)
 );
 
--- // renommer les champs des tables : exemple user_name au lieu de name
--- // recipe 
--- // position_table
--- // position_user
--- // contact 
--- // category_contact
--- // event 
--- // remind_event
-
--- à checker
 CREATE TABLE position_table (
     pos_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     pos_longitude DECIMAL(10, 5) NOT NULL,
@@ -144,20 +113,19 @@ CREATE TABLE position_table (
     pos_name VARCHAR(255)NOT NULL
 );
 
--- à checker
 CREATE TABLE position_user (
     pu_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     pu_user_id INT NOT NULL,
     pu_position_id INT NOT NULL,
-    CONSTRAINT fk_position-user_user
+    CONSTRAINT fk_position_user_user
         FOREIGN KEY (pu_user_id)
         REFERENCES user(u_id),
-    CONSTRAINT fk_position-user_position
+    CONSTRAINT fk_position_user_position
         FOREIGN KEY (pu_position_id)
         REFERENCES position_table(pos_id)
 );
 
--- à checker
+
 CREATE TABLE recipe (
     r_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     r_name VARCHAR(50) NOT NULL,
@@ -177,7 +145,7 @@ CREATE TABLE recipe (
         REFERENCES user(u_id)
 );
 
---à checker
+
 CREATE TABLE event (
      e_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
      e_title VARCHAR(50) NOT NULL,
@@ -195,26 +163,26 @@ CREATE TABLE event (
             REFERENCES group_table(g_id)
 );
 
---à checker
+
 CREATE TABLE remind_event (
     re_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     re_time TIMESTAMP NULL,
     re_message VARCHAR(255) NULL,
     re_event_id INT NOT NULL,
     re_user_id INT NOT NULL,
-        CONSTRAINT fk_remind-event_event
+        CONSTRAINT fk_remind_event_event
             FOREIGN KEY (re_event_id)
             REFERENCES event(e_id),
-        CONSTRAINT fk_remind-event_user
+        CONSTRAINT fk_remind_event_user
             FOREIGN KEY (re_user_id)
             REFERENCES user(u_id)
 );
 
---à checker
-CREATE TABLE category_contact {
+
+CREATE TABLE category_contact (
   cc_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   cc_name VARCHAR(50) NOT NULL
-}
+);
 
 
 CREATE TABLE contact (
@@ -222,11 +190,11 @@ CREATE TABLE contact (
   c_name VARCHAR(50) NOT NULL,
   c_email VARCHAR(50) NULL,
   c_phone VARCHAR(20) NULL,
-  c_address (VARCHAR255) NULL,
-  c_cat_contact_id VARCHAR(50) NULL,
+  c_address VARCHAR(255) NULL,
+  c_cat_contact_id INT NOT NULL,
   c_user_id INT NOT NULL,
   c_group_id INT NOT NULL,  
-   CONSTRAINT fk_contact_cat-contact
+   CONSTRAINT fk_contact_cat_contact
         FOREIGN KEY (c_cat_contact_id)
         REFERENCES category_contact(cc_id),
    CONSTRAINT fk_contact_user
