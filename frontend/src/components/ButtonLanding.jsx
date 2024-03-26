21aa4385884d2dbfae6c86ef3b049d827dbdeacc
ArthurVS05 <arthur.vincentsil@gmail.com>
2024-03-26 17:07:43 +0100

    présentation fonctions version mobile

--- frontend/src/components/ButtonLanding.jsx
+++ frontend/src/components/ButtonLanding.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/prop-types */
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/function-component-definition */
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ButtonLanding = ({ text, color, to }) => {
-    return (
-        <Link to={to}>
-            <button className={`mx-auto w-3/4 h-8 border-2 border-dark-default rounded-xl ${color} text-cream font-bold text-lg flex items-center justify-center font-Neue-Kabel shadow-lg md:max-w-80`} >{text}</button>
-        </Link>
-    );
-}
+  return (
+    <Link to={to}>
+      <button
+        className={`mx-auto w-3/4 h-8 border-2 border-dark-default rounded-xl ${color} text-cream font-bold text-lg flex items-center justify-center font-Neue-Kabel shadow-lg md:max-w-80`}
+      >
+        {text}
+      </button>
+    </Link>
+  );
+};
 
 export default ButtonLanding;
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,22 +1,23 @@
-
 import Bandeau from "../components/Bandeau";
 import PresentationFunctionnalities from "../components/PresentationFonctionnalities/PresentationFunctionnalities";
 import TeamComponant from "../components/TeamComponant";
 import Footer from "../components/Footer";
 import ButtonLanding from "../components/ButtonLanding";
 
-
 export default function Landing() {
   return (
     <>
-      <ButtonLanding text="Se connecter" color='bg-green-default' to="/login" />
-      <ButtonLanding text="S'inscrire" color='bg-orange-default' to="/signup" />
+      <ButtonLanding text="Se connecter" color="bg-green-default" to="/login" />
+      <ButtonLanding text="S'inscrire" color="bg-orange-default" to="/signup" />
       <Bandeau />
       <PresentationFunctionnalities />
       <TeamComponant />
-      <ButtonLanding text="Je m'inscris" color='bg-green-default' to="/signup" />
+      <ButtonLanding
+        text="Je m'inscris"
+        color="bg-green-default"
+        to="/signup"
+      />
       <Footer />
     </>
   );
-
 }
