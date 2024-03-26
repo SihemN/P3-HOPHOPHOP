f5630cc9938ebc11bb783ee51ef61510aaba2f21
ANAIS GLORENNEC <zans_69@hotmail.fr>
2024-03-26 15:58:00 +0100

    correction des fonctions fléchées

--- frontend/src/components/MenuBurgerAnchor.jsx
+++ frontend/src/components/MenuBurgerAnchor.jsx
@@ -2,36 +2,46 @@ import { useState } from "react";
 import { TiThMenu } from "react-icons/ti";
 import { IoCloseSharp } from "react-icons/io5";
 
+function MenuBurgerAnchor() {
+  const [isOpen, setIsOpen] = useState(false);
 
-const MenuBurgerAnchor = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const toggleMenu = () => setIsOpen(!isOpen)
-    const closeMenu = () => setIsOpen(false)
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
 
-    return (
-        <div className='flex justify-end p-4 font-Neue-Kabel'>
-            {!isOpen && (
-                <button onClick={toggleMenu}>
-                    <TiThMenu size={40} className="text-dark-default" />
-                </button>
-            )}
-            {isOpen && (
-                <div className="border rounded-xl animate-burgerDown">
-                    <div className="flex justify-end bg-red-default h-10 rounded-t-xl">
-                        <button onClick={closeMenu}>
-                            <IoCloseSharp size={30} className='text-cream w-10' />
-                        </button>
-                    </div>
-                    <div className="rounded-b-xl p-5 flex flex-col bg-cream text-xl ">
-                        <a className="hover:text-green-default" href="#about">A propos</a>
-                        <a className="hover:text-green-default" href="#functionality">Les fonctionnalités</a>
-                        <a className="hover:text-green-default" href="#team">La team</a>
-                    </div>
-                </div>
-            )}
+  function closeMenu() {
+    setIsOpen(false);
+  }
 
+  return (
+    <div className="flex justify-end p-4 font-Neue-Kabel">
+      {!isOpen && (
+        <button type="button" label="cliquer" onClick={toggleMenu}>
+          <TiThMenu size={40} className="text-dark-default" />
+        </button>
+      )}
+      {isOpen && (
+        <div className="border rounded-xl animate-burgerDown">
+          <div className="flex justify-end bg-red-default h-10 rounded-t-xl">
+            <button type="button" label="cliquer" onClick={closeMenu}>
+              <IoCloseSharp size={30} className="text-cream w-10" />
+            </button>
+          </div>
+          <div className="rounded-b-xl p-5 flex flex-col bg-cream text-xl">
+            <a href="#about" className="hover:text-green-default">
+              A propos
+            </a>
+            <a className="hover:text-green-default" href="#functionality">
+              Les fonctionnalités
+            </a>
+            <a className="hover:text-green-default" href="#team">
+              La team
+            </a>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default MenuBurgerAnchor;
