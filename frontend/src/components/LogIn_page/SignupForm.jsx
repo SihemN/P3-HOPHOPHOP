a98a8f277f25dec81081a0d29fbafdfe3496589b
ArthurVS05 <arthur.vincentsil@gmail.com>
2024-04-03 16:33:38 +0200

    done css login page

--- frontend/src/components/LogIn_page/SignupForm.jsx
+++ frontend/src/components/LogIn_page/SignupForm.jsx
@@ -1,40 +1,58 @@
+import { FaRegHandPointDown } from "react-icons/fa";
+import { FaCircleArrowLeft } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
 export default function SignupForm() {
   return (
-    <form className="flex flex-col text-blue-default text-xl mx-5 ">
-      <label htmlFor="name" className="font-bold">
-        Nom
-      </label>
-      <input
-        id="name"
-        type="text"
-        placeholder="Quel est votre prénom ?"
-        className="border border-solid border-blue-default h-12 mt-1 py-2 px-5 rounded-lg placeholder:text-blue-default"
-      />
-      <label htmlFor="email" className="font-bold mt-5">
-        Adresse e-mail
-      </label>
-      <input
-        id="email"
-        type="text"
-        placeholder="nom@exemple.com"
-        className="border border-solid border-blue-default h-12 mt-1 py-2 px-5 rounded-lg placeholder:text-blue-default"
-      />
-      <label htmlFor="password" className="font-bold mt-5">
-        Mot de passe
-      </label>
-      <input
-        id="password"
-        type="password"
-        placeholder="Top secret"
-        className="border border-solid border-blue-default h-12 mt-1 py-2 px-5 rounded-lg placeholder:text-blue-default"
-      />
-      <button
-        type="submit"
-        className="bg-blue-default h-12 mt-10 mb-7 py-2 px-5 rounded-lg text-cream font-semibold shadow-md shadow-dark-shadow"
-      >
-        CONTINUER
-      </button>
-      <p className="text-center">J'ai déjà un compte : me connecter</p>
-    </form>
+    <div>
+      {/* Bouton flèche retour à la landing page */}
+      <Link to="/index">
+        <FaCircleArrowLeft className="text-blue-default text-3xl mt-2" />
+      </Link>
+      <div className="flex flex-col items-center my-5">
+        <h1 className=" text-3xl font-bold text-center px-10">
+          Facilitez-vous la vie et inscrivez-vous !
+        </h1>
+        <FaRegHandPointDown className="m-3 text-3xl text-green-default" />
+      </div>
+
+      {/* Formulaire d'inscription */}
+      <form className="flex flex-col text-blue-default text-xl mx-5">
+        <label htmlFor="name" className="font-bold">
+          Prénom
+        </label>
+        <input
+          id="name"
+          type="text"
+          placeholder="Quel est votre prénom ?"
+          className="border border-solid border-blue-default h-12 mt-1 py-2 px-5 rounded-lg placeholder:text-blue-default"
+        />
+        <label htmlFor="email" className="font-bold mt-4">
+          Adresse e-mail
+        </label>
+        <input
+          id="email"
+          type="text"
+          placeholder="nom@exemple.com"
+          className="border border-solid border-blue-default h-12 mt-1 py-2 px-5 rounded-lg placeholder:text-blue-default"
+        />
+        <label htmlFor="password" className="font-bold mt-4">
+          Mot de passe
+        </label>
+        <input
+          id="password"
+          type="password"
+          placeholder="Top secret"
+          className="border border-solid border-blue-default h-12 mt-1 py-2 px-5 rounded-lg placeholder:text-blue-default"
+        />
+        <button
+          type="submit"
+          className="bg-blue-default h-12 mt-10 mb-7 py-2 px-5 rounded-lg text-cream font-semibold shadow-md shadow-dark-shadow"
+        >
+          CONTINUER
+        </button>
+        <p className="text-center">J'ai déjà un compte : me connecter</p>
+      </form>
+    </div>
   );
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,30 +1,39 @@
-import { FaCircleArrowLeft } from "react-icons/fa6";
-import { FaRegHandPointDown } from "react-icons/fa";
 import SignupForm from "../components/LogIn_page/SignupForm";
+import Logo from "../assets/logo/ghost-pink.svg";
 
 export default function Login() {
   return (
-    <div className="md:bg-red-lighter h-screen font-Neue-Kabel text-dark-default">
+    <div className="lg:bg-red-lighter h-screen font-Neue-Kabel text-dark-default ">
       {/* version PC */}
-      <div className="bg-green-lighter h-screen w-custom rounded-r-2xl shadow-lg shadow-dark-default md:block hidden">
+      <div className="bg-green-lighter h-screen w-custom rounded-r-2xl shadow-lg shadow-dark-default lg:block hidden">
         <div className="bg-orange-lighter h-screen w-custom rounded-r-2xl  shadow-lg shadow-dark-default">
-          <div className="bg-blue-default h-screen w-custom rounded-r-2xl shadow-lg shadow-dark-default">
-            {/* <SignupForm /> */}
+          <div className="bg-blue-default h-screen w-custom rounded-r-2xl shadow-lg shadow-dark-default flex justify-center items-center py-10">
+            <div className="flex justify-center items-center">
+              <div className="relative h-full">
+                {/* div background vert en arrière-plan */}
+                <div className="absolute top-[8px] left-[8px] bg-green-lighter w-10/12 h-full max-h-[650px] rounded-2xl border-solid border border-dark" />
+                {/* div 1er plan avec form et logo */}
+                <div className="relative flex flex-col justify-center bg-cream rounded-2xl  p-5 w-10/12 h-full max-h-[650px] border-solid border-[1px] border-dark-default">
+                  <SignupForm />
+                  <img
+                    src={Logo}
+                    alt="Logo fantôme Hop Hop Hop version fond rose"
+                    className="w-28 my-7 block mx-auto absolute -bottom-12 -right-10"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-
       {/* version Mobile */}
-      <div className="sm:hidden p-5">
-        {" "}
-        <FaCircleArrowLeft className="text-blue-default text-3xl" />
-        <div className="flex flex-col items-center my-5">
-          <h1 className=" text-3xl font-bold text-center">
-            Facilitez-vous la vie et inscrivez-vous !
-          </h1>
-          <FaRegHandPointDown className="m-5 text-3xl text-green-default" />
-        </div>
+      <div className="lg:hidden p-5 flex flex-col items-center justify-center">
         <SignupForm />
+        <img
+          src={Logo}
+          alt="Logo fantôme Hop Hop Hop version fond rose"
+          className="max-w-60 my-7 m-auto"
+        />
       </div>
     </div>
   );
