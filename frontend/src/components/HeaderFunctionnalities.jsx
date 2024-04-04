c0f20b0819ee8044849493c097938bf97f44f755
ANAIS GLORENNEC <zans_69@hotmail.fr>
2024-04-04 15:51:57 +0200

    create group page

--- frontend/src/components/HeaderFunctionnalities.jsx
+++ frontend/src/components/HeaderFunctionnalities.jsx
@@ -4,10 +4,27 @@ import IconHome from "./IconHome";
 // eslint-disable-next-line react/prop-types
 export default function HeaderFunctionnalities({ color, title, icon }) {
   return (
-    <div className="flex justify-between items-center p-4  ">
-      <IconHome color={color} />
-      <h1 className="text-cream font-Neue-Kabel font-bold text-2xl">{title}</h1>
-      <img src={icon} alt="" className="w-10 h-10" />
+    <div>
+      {icon && (
+        <div className="flex justify-between items-center p-4  ">
+          <IconHome color={color} />
+          <h1 className="text-cream font-Neue-Kabel font-bold text-2xl">
+            {title}
+          </h1>
+          <img src={icon} alt="" className="w-10 h-10" />
+        </div>
+      )}
+      {!icon && (
+        <div className="flex items-center p-4">
+          <div className="flex justify-start flex-grow-0">
+            <IconHome color={color} />
+          </div>
+          <h1 className="text-cream font-Neue-Kabel font-bold text-2xl flex-grow text-center">
+            {title}
+          </h1>
+          <div className="flex-grow-0" />
+        </div>
+      )}
     </div>
   );
 }
