746dcab8adefd5a5fe95d5f7ff45256fe2b249df
yuukiraa <sihem.nas51@gmail.com>
2024-04-05 08:00:58 +0200

    page home icons mobile et pc statique

---
+++ frontend/src/components/HomeNavigComp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// eslint-disable-next-line react/prop-types
+function HomeNavigComp({ title, icon }) {
+  return (
+    <div className="flex flex-col items-center justify-center bg-cream">
+      <img src={icon} alt={title} className="w-28 " />
+      <span>{title}</span>
+    </div>
+  );
+}
+
+export default HomeNavigComp;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,71 @@
 import React from "react";
 import HeaderHome from "../components/Header_home/HeaderHome";
+import HomeNavigComp from "../components/HomeNavigComp";
+
+import iconCalendar from "../assets/icons-functionnalities/calendar.svg";
+import iconBudget from "../assets/icons-functionnalities/budget.svg";
+import iconList from "../assets/icons-functionnalities/todolist.svg";
+import iconContact from "../assets/icons-functionnalities/contact.svg";
+import iconDocument from "../assets/icons-functionnalities/document.svg";
+import iconChat from "../assets/icons-functionnalities/chat.svg";
+import iconRecipe from "../assets/icons-functionnalities/recipe.svg";
+import iconLogout from "../assets/icons-functionnalities/logout.svg";
 
 export default function Home() {
+  // Définir les fonctionnalités avec les titres et les icônes correspondantes
+  const functionnalities = [
+    {
+      id: 1,
+      title: "Calendrier",
+      icon: iconCalendar,
+    },
+    {
+      id: 2,
+      title: "Budget",
+      icon: iconBudget,
+    },
+    {
+      id: 3,
+      title: "To Do List",
+      icon: iconList,
+    },
+    {
+      id: 4,
+      title: "Recettes",
+      icon: iconRecipe,
+    },
+    {
+      id: 5,
+      title: "Contacts",
+      icon: iconContact,
+    },
+    {
+      id: 6,
+      title: "Documents",
+      icon: iconDocument,
+    },
+    {
+      id: 7,
+      title: "Messagerie",
+      icon: iconChat,
+    },
+    {
+      id: 8,
+      title: "Déconnexion",
+      icon: iconLogout,
+    },
+  ];
+
   return (
-    <header>
-      <HeaderHome />
-    </header>
+    <>
+      <header>
+        <HeaderHome />
+      </header>
+      <div className="grid grid-cols-2  md:grid-cols-4 grid-rows-4 md:grid-rows-2 gap-2 m-5 ">
+        {functionnalities.map(({ id, title, icon }) => (
+          <HomeNavigComp key={id} title={title} icon={icon} />
+        ))}
+      </div>
+    </>
   );
 }
