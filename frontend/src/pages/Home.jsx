0c58de1c4ac573e8f667a71c5efe7f8ca65d0c16
ArthurVS05 <arthur.vincentsil@gmail.com>
2024-04-07 18:01:43 +0200

    correction Home.jsx et parcours Log out

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import HeaderHome from "../components/Header_home/HeaderHome";
 import HomeNavigComp from "../components/HomeNavigComp";
@@ -11,10 +11,15 @@ import iconDocument from "../assets/icons-functionnalities/document.svg";
 import iconChat from "../assets/icons-functionnalities/chat.svg";
 import iconRecipe from "../assets/icons-functionnalities/recipe.svg";
 import iconLogout from "../assets/icons-functionnalities/logout.svg";
+import Logout from "../components/Log_Out/Logout";
 
 export default function Home() {
-  // On gère si le user est connecté
-  // on récupère UserContext
+  // State qui gère l'affichage du composant Logout
+  const [iconLogoutClicked, setIconLogoutClicked] = useState(false);
+  // on gère le clic sur l'icon Déconnexion
+  const handleClick = () => {
+    setIconLogoutClicked(!iconLogoutClicked);
+  };
 
   // Définir les fonctionnalités avec les titres et les icônes correspondantes
   const functionnalities = [
@@ -22,50 +27,50 @@ export default function Home() {
       id: 1,
       title: "Calendrier",
       icon: iconCalendar,
-      Link: "/",
+      to: "/calendar",
     },
     {
       id: 2,
       title: "Budget",
       icon: iconBudget,
-      Link: "/",
+      to: "/budget",
     },
     {
       id: 3,
       title: "To Do List",
       icon: iconList,
-      Link: "/",
+      to: "/todolist",
     },
     {
       id: 4,
       title: "Recettes",
       icon: iconRecipe,
-      Link: "/",
+      to: "/recipes",
     },
     {
       id: 5,
       title: "Contacts",
       icon: iconContact,
-      Link: "/",
+      to: "/contacts",
     },
     {
       id: 6,
       title: "Documents",
       icon: iconDocument,
-      Link: "/",
+      to: "/documents",
     },
     {
       id: 7,
       title: "Messagerie",
       icon: iconChat,
-      Link: "/",
+      to: "/chat",
     },
-    {
-      id: 8,
-      title: "Déconnexion",
-      icon: iconLogout,
-      Link: "/",
-    },
+    // {
+    //   id: 8,
+    //   title: "Déconnexion",
+    //   icon: iconLogout,
+    //   Link: "/",
+    // },
   ];
 
   return (
@@ -75,11 +80,22 @@ export default function Home() {
       </header>
       <div className="rounded-t-3xl lg:rounded-t-[4rem] bg-cream h-custom flex justify-center items-center">
         <div className="grid grid-cols-2 lg:grid-cols-4 grid-rows-4 lg:grid-rows-2 gap-8 lg:gap-16 md:gap-20 max-w-[75%] py-3 px-2 lg:p-10">
-          {functionnalities.map(({ id, title, icon }) => (
-            <Link to={Link} key={id}>
+          {functionnalities.map(({ id, title, icon, to }) => (
+            <Link to={to} key={id}>
               <HomeNavigComp title={title} icon={icon} />
             </Link>
           ))}
+          <div className="relative">
+            {" "}
+            <button
+              type="button"
+              aria-label="icon déconnexion"
+              onClick={handleClick}
+            >
+              <HomeNavigComp title="Déconnexion" icon={iconLogout} />
+            </button>
+            {iconLogoutClicked && <Logout handleClick={handleClick} />}
+          </div>
         </div>
       </div>
     </div>
